ba82f0f49ca5bc8e0f8ca78187aac7d928c53098
Tom Hipkin <tomhipkin@gmail.com>
2020-08-20 10:28:56 +0100

    Refactor the stubbing of dalmatian files into reusable method
    I'd like to start using the same steps in more tests. Instead of using a before block and throwing it in there as a mystery guest I prefer to call it explicity from within each test with a method call.

--- spec/features/users_can_see_all_infrastructures_spec.rb
+++ spec/features/users_can_see_all_infrastructures_spec.rb
@@ -1,5 +1,15 @@
 feature "Users can see all infrastructures" do
   scenario "lists out the names of each infrastructure" do
+    stub_infrastructures_file_load
+
+    visit root_path
+
+    expect(page).to have_content(I18n.t("app.name"))
+    expect(page).to have_content(I18n.t("page_title.infrastructures"))
+    expect(page).to have_content("test-app")
+  end
+
+  def stub_infrastructures_file_load
     config = YAML.safe_load(File.read("spec/fixtures/dalmatian-config/dalmatian.yml"))
     dalmatian_configuration_double = instance_double(FindDalmatianConfiguration)
     allow(FindDalmatianConfiguration)
@@ -8,11 +18,5 @@ feature "Users can see all infrastructures" do
     allow(dalmatian_configuration_double)
       .to receive(:infrastructures)
       .and_return(config["infrastructures"])
-
-    visit root_path
-
-    expect(page).to have_content(I18n.t("app.name"))
-    expect(page).to have_content(I18n.t("page_title.infrastructures"))
-    expect(page).to have_content("test-app")
   end
 end
