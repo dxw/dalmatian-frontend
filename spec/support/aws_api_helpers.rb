cf28e13f9588190f578155beccdd54ef32875417
Tom Hipkin <tomhipkin@gmail.com>
2020-09-11 16:10:49 +0100

    Refactor AWS API spec helpers so that client stubbing is decoupled
    Before we add in support for the stubbing of a new API method, refactor the client stubbing into their own methods for easier reuse.
    
    The aws_sts_client can remain private however stub_aws_ssm_client will need to be public for the logic in find_environment_variables_spec. It requires that 4 requests are stubbed (for each service and environment), to so this we need to ensure the client is the same double. Exposing stub_aws_ssm_client as a public method allows this to happen.

--- spec/support/aws_api_helpers.rb
+++ spec/support/aws_api_helpers.rb
@@ -1,31 +1,43 @@
 module AwsApiHelpers
   def stub_call_to_aws_for_environment_variables(
     account_id:,
-    aws_client_double: instance_double(Aws::SSM::Client),
+    aws_ssm_client_double: nil,
     request_path:,
     environment_variables:
   )
-    client = instance_double(Aws::STS::Client)
-    allow(Aws::STS::Client).to receive(:new).and_return(client)
+    aws_ssm_client = aws_ssm_client_double || stub_aws_ssm_client(account_id: account_id)
 
-    credentials = instance_double(Aws::AssumeRoleCredentials)
-    allow(Aws::AssumeRoleCredentials).to receive(:new).with(
-      client: client,
-      role_arn: "arn:aws:iam::#{account_id}:role/#{ENV["AWS_ROLE"]}",
-      role_session_name: "role_session_name"
-    ).and_return(credentials)
-
-    allow(Aws::SSM::Client)
-      .to receive(:new)
-      .with(credentials: credentials)
-      .and_return(aws_client_double)
-
-    allow(aws_client_double)
+    allow(aws_ssm_client)
       .to receive(:get_parameters_by_path)
       .with(
         path: request_path,
         with_decryption: true,
         recursive: false
       ).and_return(environment_variables).once
+  end
+
+  def stub_aws_ssm_client(account_id:)
+    credentials = instance_double(Aws::AssumeRoleCredentials)
+    allow(Aws::AssumeRoleCredentials).to receive(:new).with(
+      client: aws_sts_client,
+      role_arn: "arn:aws:iam::#{account_id}:role/#{ENV["AWS_ROLE"]}",
+      role_session_name: "role_session_name"
+    ).and_return(credentials)
+
+    aws_ssm_client_double = instance_double(Aws::SSM::Client)
+    allow(Aws::SSM::Client)
+      .to receive(:new)
+      .with(credentials: credentials)
+      .and_return(aws_ssm_client_double)
+
+    aws_ssm_client_double
+  end
+
+  private
+
+  def aws_sts_client
+    aws_sts_client = instance_double(Aws::STS::Client)
+    allow(Aws::STS::Client).to receive(:new).and_return(aws_sts_client)
+    aws_sts_client
   end
 end
