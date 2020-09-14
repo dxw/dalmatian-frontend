51cbf25d9dee9dca9aac90cbc0f0435471cc11ec
Tom Hipkin <tomhipkin@gmail.com>
2020-09-14 13:21:41 +0100

    Throw away checks to ensure variables are only deleted for the intended service
    As there is no formal data association between Infrastructure and EnvironmentVariable the state is passed around with query params.
    
    Due to this choice I had added some checks to try to assert if those query params had been changed. For example, if the create or delete action was called for staging but somehow changed to be applied to production, guard against it.
    
    In order to make this approach work for the #delete action as well as the #create action I set off down a rabbit hole of trying to use a before_action. Given how the parameters are provided in different ways this quickly led me to question the need for this logic at all. I have instead added the service name and environment name that is in the form request, into the confirmation box. This will hopefully provide confidence and solve the problem of catching any unexpected code changes which breaks the way we send this state around.

--- app/controllers/environment_variables_controller.rb
+++ app/controllers/environment_variables_controller.rb
@@ -11,7 +11,7 @@ class EnvironmentVariablesController < ApplicationController
 
     @environment_variable = EnvironmentVariable.new(environment_variable_params)
 
-    if @environment_variable.valid? && safe_service_name? && safe_environment_name?
+    if @environment_variable.valid?
       CreateEnvironmentVariable.new(infrastructure: @infrastructure)
         .call(environment_variable: @environment_variable)
       redirect_to infrastructure_path(@infrastructure)
@@ -42,15 +42,5 @@ class EnvironmentVariablesController < ApplicationController
 
   def value
     environment_variable_params[:value]
-  end
-
-  def safe_environment_name?
-    raise ArgumentError unless @infrastructure.environment_names.include?(environment_variable_params[:environment_name])
-    true
-  end
-
-  def safe_service_name?
-    raise ArgumentError unless @infrastructure.service_names.include?(environment_variable_params[:service_name])
-    true
   end
 end
