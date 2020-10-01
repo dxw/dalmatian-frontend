66da4d5adb0fe0361c8293e5659860ec91ff5267
Tom Hipkin <tomhipkin@gmail.com>
2020-10-01 15:22:05 +0100

    Display a flash error when the local AWS token has expired
    I was unable to find a way to test this given the way the AWS gem initiliases itself.
    
    When we write `Aws::STS:Client.new` with no arguments the gem will go off and load the credentials from your file system [1]. What it will also do is reinitialise itself after loading in these aguments. To successfully stub a fake exception here I would either have to pass in the credentials directly and lose parity with how it's really done or I'd have to dig deeper into the gem in terms of stubbing. This latter option took me down an unsuccessful rabbit hole.
    
    [1] https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/STS/Client.html#initialize-instance_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  rescue_from Aws::STS::Errors::ExpiredToken, with: :expired_token
+
   def health_check
     render json: {rails: "OK"}, status: :ok
+  end
+
+  private
+
+  def expired_token
+    @error_message = "Your AWS token has expired. This happens every 12 hours. You can renew it by following <a href='https://github.com/dxw/dalmatian-frontend#prerequisites' target='_blank'>the dalmatian-mfa steps in the readme</a>."
+    render "pages/error", status: 500
   end
 end
